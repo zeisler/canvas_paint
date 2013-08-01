4aed94481c8b41883b914dece8306b8ed8d8e30a
Dustin Zeisler <dustin@zive.me>
2013-08-01 12:33:02 -0700

    user can change thickness of paint brush and clear canvas

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -7,7 +7,7 @@ angular.module('canvasPaintApp')
     ctx.clearRect(0,0, canvas.width, canvas.height);
     var centerX = canvas.width / 2;
     var centerY = canvas.height / 2;
-
+    $scope.thickness = 10;
 
      function getMousePos(canvas, evt) {
         var rect = canvas.getBoundingClientRect();
@@ -21,9 +21,14 @@ angular.module('canvasPaintApp')
     $('#canvas').on('mousemove', function(evt) {
         var mousePos = getMousePos(canvas, evt);
             ctx.beginPath();
-            ctx.arc(mousePos.x, mousePos.y, 3,0,Math.PI*2,true);
+            ctx.arc(mousePos.x, mousePos.y, $scope.thickness,0,Math.PI*2,true);
             ctx.closePath();
             ctx.fillStyle = 'green';
             ctx.fill();
       });
+    $scope.clear = function(){
+      var canvas = document.getElementById('canvas');
+      var ctx = canvas.getContext('2d');
+      ctx.clearRect(0,0, canvas.width, canvas.height);
+    }
   });
